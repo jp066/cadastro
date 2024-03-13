f5d4be1b25d40f5fbd4ba335a2609ee703e5037d
joao pedro matos <jpedro030506@gmail.com>
2024-03-13 15:40:07 -0300

    commit

--- cadastro.dart
+++ cadastro.dart
@@ -4,40 +4,53 @@ class Funcionarios{
  String? nomeFuncionario;
  int cpf;
  int _idCadastroFuncionarios;
- double _salario;
 
- Funcionarios(this.nomeFuncionario, this.cpf, this._idCadastroFuncionarios, this._salario);
+ Funcionarios(this.nomeFuncionario, this.cpf, this._idCadastroFuncionarios);
   
   int get idCadastroFuncionarios => _idCadastroFuncionarios;
   set idCadastroFuncionarios(int value) => _idCadastroFuncionarios = value;
-
-  double get salario => _salario;
-  set salario(double value) => _salario = value;
-
 
  void detalhesFuncionarios(){
   print('nome: $nomeFuncionario');
   print('Cpf: $cpf');
   print('Id: $_idCadastroFuncionarios');
-  print('seu salario é: $_salario');
-
  }
 }
 
+class Tarefas extends Funcionarios{
+  String? tarefas;
+
+  Tarefas(this.tarefas,super.nomeFuncionario, super.cpf, super.idCadastroFuncionarios);
+
+  void afazeres(){
+    for(int i = 0 ; i >= 0; i++ ){
+      return print('$nomeFuncionario, sua tarefa hoje é $tarefas');
+    }
+  }
+
+}
+
 class Clientes{
-  String nomeclientes;
+  String? nomeClientes;
   int _idCadastroClientes;
+  int opcao = int.parse(stdin.readLineSync()!);
+
   
-  Clientes(this._idCadastroClientes, this.nomeclientes);
+  Clientes(this.nomeClientes, this._idCadastroClientes, this.opcao);
   
   int get idCadastroClientes => _idCadastroClientes;
   set idCadastroFuncionarios(int value) => _idCadastroClientes = value;
 
+  
+  void chamar(){
+    if(opcao == 1){
+      return detalhesClientes();
+    }
+  }
+
   void detalhesClientes(){
-
+    print('nome: $nomeClientes');
     print('id: $idCadastroClientes');
-    print('Cliente: $nomeclientes');
-
   }
 }
 class Produtos {
@@ -46,30 +59,57 @@ class Produtos {
 
   Produtos(this.idProduto, this.setor);
 
-  void detalhes(){
-    print('o id do produto é $idProduto  e seu setor é $setor');
+  void detalhesProdutos(){
+    print('id: $idProduto');
+    print('setor: $setor');
   }
 }
 
 void main(){
   //funcionarios
+  print('bem vindo, voce é: ');
+  print('1.funcionario');
+  print('2.cliente');
+  int opcao = int.parse(stdin.readLineSync()!);
+  
+if(opcao == 1){
   print('digite seu nome: ');
   String? nomeFuncionario = stdin.readLineSync();
   print('digite seu cpf: ');
   int cpf = int.parse(stdin.readLineSync()!);
   print('seu Id é: ');
   int _idCadastroFuncionarios = int.parse(stdin.readLineSync()!);
-  print('seu _salario é: ');
-  double _salario = double.parse(stdin.readLineSync()!);
+  print('---------------------------------------------------------------------------------------');
+  print('Rede SESI');
+  print('---------------------------------------------------------------------------------------');
+  print('bem vindo $nomeFuncionario, o que voce quer fazer hoje?');
 
+
+  //instancias de funcionarios
+  Funcionarios funcionarios = Funcionarios(nomeFuncionario, cpf, _idCadastroFuncionarios);
+  funcionarios.detalhesFuncionarios();
+}
+else if(opcao ==2){
   //clientes
   print('digite seu nome: ');
   String? nomeClientes = stdin.readLineSync();
   print('seu Id é: ');
-  int _idCadastroFuncionarios = int.parse(stdin.readLineSync()!);
-  print('seu _salario é: ');
-  double _salario = double.parse(stdin.readLineSync()!);
+  int _idCadastroClientes = int.parse(stdin.readLineSync()!);
+  print('----------------------------------------------------------------------------------------');
+  print('seja bem vindo ao sistema SESI, $nomeClientes.');
+  print('o que voce deseja?');
 
-  Funcionarios funcionarios = Funcionarios(nomeFuncionario, cpf, _idCadastroFuncionarios, _salario);
-  funcionarios.detalhesFuncionarios();
+  //instancias de clientes
+  Clientes clientes = Clientes(nomeClientes, _idCadastroClientes, opcao);
+  clientes.detalhesClientes();
 }
+  //produtos
+  Produtos produtos1 = Produtos(1, 'amazonas');
+  Produtos produtos2 = Produtos(2, 'piaui');
+  Produtos produtos3 = Produtos(3, 'cuiaba');
+  Produtos produtos4 = Produtos(4, 'bahia');
+  produtos1.detalhesProdutos();
+  produtos2.detalhesProdutos();
+  produtos3.detalhesProdutos();
+  produtos4.detalhesProdutos();
+  }
